43d6bce54d290bfd296352117bd652c7f280943f
Delighted-Divide <flamingicebtw@gmail.com>
2023-09-27 18:09:25 +0530

    marking once clicked

--- trial_js.js
+++ trial_js.js
@@ -5,6 +5,12 @@ var icons = document.querySelectorAll(".start li i");
 icons.forEach(function (icon) {
   icon.addEventListener("click", function () {
     // Toggle the 'icon-active' class on the clicked icon
+    icons.forEach(function (inactiveIcon) {
+      inactiveIcon.classList.remove("icon-active");
+      inactiveIcon.parentElement.classList.remove("li-active");
+    });
+
     this.classList.toggle("icon-active");
+    this.parentElement.classList.add("li-active");
   });
 });
